adf29390d47685d05223db6fe54286813894a4f0
jitender.rana <jitender.rana@delphix.com>
2022-07-19 19:54:40 +0530

    confluence

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from datetime import datetime
 from confluence import *
+from slack_sdk import WebClient
 
 
 APP_TOKEN = "xapp-1-A03NNN2H4E5-3751520877734-f768a856842e41c888bee8001d97493ebaabf6999f3c82bc8e88761cb73012db"
@@ -13,18 +14,30 @@ app = App(token=BOT_TOKEN)
 
 @app.event("app_mention")
 def handle_app_mention_events(body: dict, say: Callable):
-    # print(body)
-    say("Done")
-    bot_id = body.get("event", {}).get("text").split()[0]
-    message = body.get("event", {}).get("text").replace(bot_id, "").strip()
-    print(message)
-    event_time = body["event_time"]
-    subpage_name = datetime.fromtimestamp(event_time).strftime('%B-%Y')
-    print(subpage_name)
-    page_id = get_page_id(subpage_name)
-    if page_id is None:
-        page_id = create_page(subpage_name)
-    update_page(page_id, message, 'Recognitions')
+    print(body)
+    try:
+        bot_id = body.get("event", {}).get("text").split()[0]
+        message = body.get("event", {}).get("text").replace(bot_id, "").strip()
+        channel = body.get("event", {}).get("channel")
+        user = body.get("event", {}).get("user")
+        print(message)
+        event_time = body["event_time"]
+        subpage_name = datetime.fromtimestamp(event_time).strftime('%B-%Y')
+        print(subpage_name)
+        page_id = get_page_id(subpage_name)
+        if page_id is None:
+            page_id = create_page(subpage_name)
+        update_page(page_id, message, 'Recognitions')
+    except BaseException as exp:
+        raise exp
+    else:
+        # say('Done')
+        client = WebClient(token=BOT_TOKEN)
+        response = client.chat_postEphemeral(
+            channel=channel,
+            text="Your update has been successfully appended to Newsletter",
+            user=user,
+        )
 
 
 if __name__ == "__main__":
