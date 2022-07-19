f024d25381bb9ef7abd989f2822f8145f13937b0
jitender.rana <jitender.rana@delphix.com>
2022-07-19 19:54:40 +0530

    confluence

--- src/app.py
+++ src/app.py
@@ -14,26 +14,24 @@ app = App(token=BOT_TOKEN)
 
 @app.event("app_mention")
 def handle_app_mention_events(body: dict, say: Callable):
-    print(body)
     try:
         bot_id = body.get("event", {}).get("text").split()[0]
         message = body.get("event", {}).get("text").replace(bot_id, "").strip()
         channel = body.get("event", {}).get("channel")
         user = body.get("event", {}).get("user")
-        print(message)
+
         event_time = body["event_time"]
         subpage_name = datetime.fromtimestamp(event_time).strftime('%B-%Y')
-        print(subpage_name)
         page_id = get_page_id(subpage_name)
+
         if page_id is None:
             page_id = create_page(subpage_name)
         update_page(page_id, message, 'Recognitions')
     except BaseException as exp:
         raise exp
     else:
-        # say('Done')
         client = WebClient(token=BOT_TOKEN)
-        response = client.chat_postEphemeral(
+        client.chat_postEphemeral(
             channel=channel,
             text="Your update has been successfully appended to Newsletter",
             user=user,
